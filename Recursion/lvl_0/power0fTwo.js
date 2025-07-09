e2f1cd1bd3192e7ff42d97c767ac28da5352bc30
saratnayak0808 <saratnayak0808@gmail.com>
2025-07-09 22:53:32 +0530

    power of two

--- Recursion/lvl_0/power0fTwo.js
+++ Recursion/lvl_0/power0fTwo.js
@@ -1,17 +1,21 @@
-var isPowerOfTwo = function(n) {
-    // if (n <= 0) return false; 
-    // if(n==1) return true;
-    // if(n%2=== 0){
-    //     return isPowerOfTwo(n/2)
-    // }else{
-    //     return false;
-    // }
+//231. Power of Two
 
+var isPowerOfTwo = function (n) {
+  // if (n <= 0) return false;
+  // if(n==1) return true;
+  // if(n%2=== 0){
+  //     return isPowerOfTwo(n/2)
+  // }else{
+  //     return false;
+  // }
 
-    if(n<=0) return false;
-    return !(n&(n-1));
-     
+  if (n <= 0) return false;
+  return !(n & (n - 1));
 };
 
-let n=16;
+//test case
+let n = 16;
 console.log(isPowerOfTwo(n));
+
+// link
+// https://leetcode.com/problems/power-of-two/description/
