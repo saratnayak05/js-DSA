1fb5a8fb20a3666c6678f2caad6e1fd894459b0a
saratnayak0808 <saratnayak0808@gmail.com>
2025-07-13 13:28:32 +0530

    Intersection of Two Linked Lists

--- LinkedList/lvl_0/findIntersectionPointLL.js
+++ LinkedList/lvl_0/findIntersectionPointLL.js
@@ -1,8 +1,5 @@
 // 160. Intersection of Two Linked Lists
 
-
-
-
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
@@ -16,60 +13,45 @@
  * @param {ListNode} headB
  * @return {ListNode}
  */
-var getIntersectionNode = function(headA, headB) {
+var getIntersectionNode = function (headA, headB) {
+  //brute-force approch ------T.C O(n^m) S.C O(1)
+  //  let curr1 = headA;
 
+  // while (curr1) {
+  //     let curr2 = headB;
+  //     while (curr2) {
+  //         if (curr1 === curr2) return curr1;
+  //         curr2 = curr2.next;
+  //     }
+  //     curr1 = curr1.next;
+  // }
 
-    //brute-force approch ------T.C O(n^m) S.C O(1)
-    //  let curr1 = headA;
+  // return null;
 
-    // while (curr1) {
-    //     let curr2 = headB;
-    //     while (curr2) {
-    //         if (curr1 === curr2) return curr1;
-    //         curr2 = curr2.next;
-    //     }
-    //     curr1 = curr1.next;
-    // }
+  // (Optimal )two pointer approch--T.C O(n+m) S.C O(1)
+  let curr1 = headA;
+  let curr2 = headB;
 
-    // return null;
+  while (curr1 !== curr2) {
+    curr1 = curr1 ? curr1.next : headB;
+    curr2 = curr2 ? curr2.next : headA;
+  }
+  return curr1;
 
-
-
-    // (Optimal )two pointer approch--T.C O(n+m) S.C O(1)
-    let curr1=headA;
-    let curr2=headB;
-    
-    while(curr1 !== curr2){
-       curr1=curr1?curr1.next : headB;
-       curr2=curr2? curr2.next : headA;
-    }
-    return curr1;
-
-
-
-    //using set--- T.C O(n+m) S.C O(n)
-//      let setB = new Set();
-//      let curr2=headB;
-//       while(curr2){
-//         setB.add(curr2);
-//         curr2=curr2.next;
-//       }
-//       let curr1=headA;
-//       while(curr1){
-//         if(setB.has(curr1)){return curr1}
-//         curr1=curr1.next;
-//       }
-//       return null;
-
-
-
+  //using set--- T.C O(n+m) S.C O(n)
+  //      let setB = new Set();
+  //      let curr2=headB;
+  //       while(curr2){
+  //         setB.add(curr2);
+  //         curr2=curr2.next;
+  //       }
+  //       let curr1=headA;
+  //       while(curr1){
+  //         if(setB.has(curr1)){return curr1}
+  //         curr1=curr1.next;
+  //       }
+  //       return null;
 };
-
-
-
-
-
-
 
 // link
 // https://leetcode.com/problems/intersection-of-two-linked-lists/description/
